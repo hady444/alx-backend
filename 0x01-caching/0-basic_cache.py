bca90b78145fd9e0c110c3bafb701d127b6b598a
hady444 <hady444@github.com>
2024-11-02 12:12:49 +0200

    c

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,20 @@
+from base_caching import BaseCaching
+"""
+Module
+"""
+
+
+class BasicCache(BaseCaching):
+    """Represents an object that allows storing and
+    retrieving items from a dictionary.
+    """
+    def put(self, key, item):
+        """
+        add item cached
+        """
+        if key and item:
+            self.cache_data[key] = item
+    def get(self, key):
+        """Retrieves an item by key.
+        """
+        return self.cache_data.get(key, None)
